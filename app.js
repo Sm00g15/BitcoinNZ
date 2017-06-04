ebaa06818ab0c62de3d4b8ba4d0ab234397095e7
Sean M <smagin07@Seans-MacBook-Air.local>
2017-06-04 16:23:34 +1200

    final product v1

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+var request = require("request"); // must require request for any API work
+app.set("view engine", "ejs");
+app.use(express.static("public")); //serve up custom style sheet
+
+
+app.get("/", function(req, res){
+	request("https://localbitcoins.com/bitcoinaverage/ticker-all-currencies/", function(error, response, body){
+		if(!error && response.statusCode == 200){
+			var data = JSON.parse(body)
+			res.render("index", {data: data});
+		}
+	});
+});
+
+app.listen(3000, function(){
+    console.log("Bitcoin Template Has Started");
+});
